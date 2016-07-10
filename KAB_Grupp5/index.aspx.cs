fc1c191553722b5e5c9a94151c24a752e0c76c41
Administrator <Administrator@ACADEMY-6162X31>
2016-07-10 14:36:52 +0200

    La till databas och connection string i index.aspx.cs . Kommer behöva era IP-adresser så ni får tillgång till databasen!

--- KAB_Grupp5/index.aspx.cs
+++ KAB_Grupp5/index.aspx.cs
@@ -9,8 +9,12 @@ namespace KAB_Grupp5
 {
     public partial class index : System.Web.UI.Page
     {
+        // Server connection string to database KABdatabase
+        static string conStr = "Data Source=grupp5projekt.database.windows.net;Initial Catalog = KABdatabase; Integrated Security = False; User ID = grupp5projekt; Password=AWAgrupp5;Connect Timeout = 15; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
 
         }
     }
